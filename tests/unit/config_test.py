4152d1a97fda4fa3b6ac9ae6fef3f4bdf1a90214
Thomas Lacroix <thomas.lacroix@outlook.com>
2014-07-30 15:56:19 +0200

    refactor(test): Change config loading
    Load the yaml file in the module setup, create the objects from the
    data in the class set up

--- tests/unit/config_test.py
+++ tests/unit/config_test.py
@@ -1,15 +1,25 @@
 import unittest
-from config import Config, NoServiceException, NoArtifactException
+import yaml
+import os
+from config import Config, ServiceConfig, NoServiceException, NoArtifactException
 from plan import CharacteristicSpecification
 
 def setUpModule():
-    global config
-    config = Config.from_path('tests/fixtures/config')
+    global artifacts
+    global services
+    global config_fixtures_dir 
+
+    config_fixtures_dir = os.path.join('tests', 'fixtures', 'config')
+    with open(os.path.join(config_fixtures_dir, 'services', 'services.yaml')) as f:
+        services = yaml.load(f)
+
+    with open(os.path.join(config_fixtures_dir, 'artifacts', 'artifacts.yaml')) as f:
+        artifacts = yaml.load(f)
 
 class ConfigTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.config = config
+        cls.config = Config.from_path(config_fixtures_dir)
     
     def test_number_services(self):
         self.assertEqual(len(self.config.services), 2)
@@ -42,7 +52,12 @@ class ConfigTest(unittest.TestCase):
         expected = self.config.services[0]
         self.assertEqual(actual, expected)
 
-    def test_find_nin_existing_service_by_characteristics(self):
+    def test_find_existing_service_by_empty_characteristics(self):
+        actual = self.config.find_service_by_characteristics([])
+        expected = self.config.services[0]
+        self.assertEqual(actual, expected)
+
+    def test_find_non_existing_service_by_characteristics(self):
         characteristics = [CharacteristicSpecification(**{'characteristic_type': 'MongoDB', 'MongoDB:version': "2.6"})]
         with self.assertRaises(NoServiceException):
             self.config.find_service_by_characteristics(characteristics)
