e68398e2a2f3fa923025936ad8b344bcd71b0533
Thomas Lacroix <thomas.lacroix@outlook.com>
2014-07-30 09:44:07 +0200

    test(config): Test the Config class

--- tests/unit/config_test.py
+++ tests/unit/config_test.py
@@ -1,23 +1,44 @@
 import unittest
-from config import Config
+from config import Config, NoServiceException, NoArtifactException
+from plan import CharacteristicSpecification
 
 class ConfigTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.config = Config()
+        cls.config = Config(path='tests/fixtures/config')
     
-    def test_services(self):
-        services = self.config.services
-        self.assertEqual(len(services), 2)
-        self.assertEqual(services[0].name, "mongodb")
-        self.assertEqual(services[0].base, "dockerfile/mongodb")
-        self.assertEqual(services[0].type, "database")
-        self.assertEqual(len(services[0].characteristics), 3)
+    def test_number_services(self):
+        self.assertEqual(len(self.config.services), 2)
 
-    def test_artifacts(self):
-        artifacts = self.config.artifacts
-        self.assertEqual(len(artifacts),2)
-        self.assertEqual(artifacts[0].name, "Nodejs:Application")
-        self.assertEqual(artifacts[0].default_requirement, "Nodejs:Run")
-        self.assertEqual(len(artifacts[0].requirements), 1)
-        
+    def test_number_artifacts(self):
+        self.assertEqual(len(self.config.artifacts),  2)
+
+    def test_find_existing_artifact(self):
+        actual = self.config.find_artifact_config_by_type("Nodejs:Application")
+        expected = self.config.artifacts[0]
+        self.assertEqual(actual, expected)
+
+    def test_find_non_existing_artifact(self):
+        with self.assertRaises(NoArtifactException):
+            self.config.find_artifact_config_by_type("Cat")
+
+    def test_find_existing_service_by_name(self):
+        actual = self.config.find_service_by_name("mongodb")
+        expected = self.config.services[0]
+        self.assertEqual(actual, expected)
+
+    def test_find_non_existing_service_by_name(self):
+        with self.assertRaises(NoServiceException):
+            self.config.find_service_by_name("Restaurant")
+
+    def test_find_existing_service_by_characteristics(self):
+        """Test the search of a latest MongoDB database service"""
+        characteristics = [CharacteristicSpecification(**{'characteristic_type': 'MongoDB', 'MongoDB:version': 'latest'}), CharacteristicSpecification(**{'characteristic_type': 'Database'})]
+        actual = self.config.find_service_by_characteristics(characteristics)
+        expected = self.config.services[0]
+        self.assertEqual(actual, expected)
+
+    def test_find_nin_existing_service_by_characteristics(self):
+        characteristics = [CharacteristicSpecification(**{'characteristic_type': 'MongoDB', 'MongoDB:version': "2.6"})]
+        with self.assertRaises(NoServiceException):
+            self.config.find_service_by_characteristics(characteristics)
